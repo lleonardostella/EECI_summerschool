f717f38504ccb6627ae713781b4e5a2f3b5627ec
lleonardostella <lleonardo.stella@gmail.com>
2024-04-17 18:37:20 +0100

    updated Evolutionary_Tournament

--- Evolutionary_Tournament.py
+++ Evolutionary_Tournament.py
@@ -2,15 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random as rand
 
-# Initialising time horizon
-T = rand.randrange(100, 1000, 1)
+def evolutionary_tournament(N,T,p,move1,move2,payoff_matrix,stag_hunts):
+    for i in range(0,N-1):
+        for j in range(i+1,N):
+            move1[0] = stag_hunts[i](2)
+            move2[0] = stag_hunts[j](2)
 
-# Creating the arrays of moves, 0 = hare, 1 = stag, 2 = first move
-move1 = np.zeros(T, dtype=int)
-move2 = np.zeros(T, dtype=int)
+            p[i,0] = p[i,0] + payoff_matrix[move1[0],move2[0]]
+            p[j,0] = p[j,0] + payoff_matrix[move2[0],move1[0]]
 
-# Define the payoff matrix
-payoff_matrix = np.array([[4, 1], [3, 2]])
+            for t in range(1,T):
+                move1[t] = stag_hunts[i](move2[t-1])
+                move2[t] = stag_hunts[j](move1[t-1])
+
+                p[i,t] = p[i,t-1] + payoff_matrix[move1[t],move2[t]]
+                p[j,t] = p[j,t-1] + payoff_matrix[move2[t],move1[t]]
+
+            if j != N-1:
+                p[i,0] = p[i,T-1]
+            if i != N-2:
+                p[j,0] = p[j,T-1]
+
+    
+    print('The number of rounds for this tournament is: ',T)
+    for i in range(0,N):
+        print('The total payoff of player ', i+1, ' is ', p[i,T-1])
+        plt.plot(p[i,:], label='Player '+str(i+1))
+
+    plt.xlabel('Iteration: t')
+    plt.ylabel('Cumulative Payoff: x')
+    plt.title('Evolutionary Tournament: Prisoner\'s Dilemma')
+    plt.legend()
+    plt.grid()
+    plt.show()
 
 """ TESTING
 def stag_hunt1(move):
@@ -57,7 +81,7 @@ def strategy_RAP(opponent_action):
        else:
            return opponent_action    
    else:
-       if rand.uniform(0,1) >= 0.5:
+       if rand.uniform(0,1) >= 0.1:
            return 0
        else:
            return opponent_action
@@ -77,44 +101,23 @@ def maximise(input):
         return 1
 
 
-
 stag_hunts = [tik_tok, JE_strategy, strategy_RAP, select_strategy, maximise]
-# for func in list_of_functions:
-#    func()
 N = len(stag_hunts)
+
+# Initialising time horizon
+T = rand.randrange(100, 1000, 1)
 
 # Creating an array of integers of size N (\in \mathbb Z)
 p = np.zeros((N, T), dtype=int)
 
-for i in range(0,N-1):
-    for j in range(i+1,N):
-        move1[0] = stag_hunts[i](2)
-        move2[0] = stag_hunts[j](2)
+# Creating the arrays of moves, 0 = hare, 1 = stag, 2 = first move
+move1 = np.zeros(T, dtype=int)
+move2 = np.zeros(T, dtype=int)
 
-        p[i,0] = p[i,0] + payoff_matrix[move1[0],move2[0]]
-        p[j,0] = p[j,0] + payoff_matrix[move2[0],move1[0]]
+# Define the payoff matrix
+payoff_matrix_SH = np.array([[2, 3], [1, 4]])
 
-        for t in range(1,T):
-            move1[t] = stag_hunts[i](move2[t-1])
-            move2[t] = stag_hunts[j](move1[t-1])
+evolutionary_tournament(N,T,p,move1,move2,payoff_matrix_SH,stag_hunts)
 
-            p[i,t] = p[i,t-1] + payoff_matrix[move1[t],move2[t]]
-            p[j,t] = p[j,t-1] + payoff_matrix[move2[t],move1[t]]
-
-        if j != N-1:
-            p[i,0] = p[i,T-1]
-        if i != N-2:
-            p[j,0] = p[j,T-1]
-
-    
-print('The number of rounds for this tournament is: ',T)
-for i in range(0,N):
-    print('The total payoff of player ', i+1, ' is ', p[i,T-1])
-    plt.plot(p[i,:], label='Player '+str(i+1))
-
-plt.xlabel('Iteration: t')
-plt.ylabel('Cumulative Payoff: x')
-plt.title('Evolutionary Tournament: Prisoner\'s Dilemma')
-plt.legend()
-plt.grid()
-plt.show()
+#stag_hunts = [tik_tok, tik_tok, tik_tok, tik_tok, tik_tok]
+#evolutionary_tournament(N,T,p,move1,move2,payoff_matrix_SH,stag_hunts)
